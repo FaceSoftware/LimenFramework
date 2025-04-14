5d88f96ca5ab8babb6817a796cf2e844986b73d7
José Parente <j.eduardo.parente@gmail.com>
2025-04-14 23:02:59 +0100

    Minor changes to AmmoBox class

--- LimenInteraction/Source/LimenWeapons/Private/Actors/LimenAmmoBox.cpp
+++ LimenInteraction/Source/LimenWeapons/Private/Actors/LimenAmmoBox.cpp
@@ -4,6 +4,8 @@
 #include "Actors/LimenAmmoBox.h"
 
 #include "Components/Interaction/LimenInteractionComponent.h"
+#include "Engine/World.h"
+#include "GameFramework/Pawn.h"
 
 
 ALimenAmmoBox::ALimenAmmoBox()
@@ -11,35 +13,21 @@ ALimenAmmoBox::ALimenAmmoBox()
 	AmmoCount = 1;
 }
 
-void ALimenAmmoBox::BeginPlay()
-{
-	Super::BeginPlay();
-
-	AmmoInstances.Reserve(AmmoCount);
-	for (int i = 0; i < AmmoCount; ++i)
-	{
-		ALimenAmmo* AmmoInstance = GetWorld()->SpawnActor<ALimenAmmo>(AmmoClass);
-		AmmoInstance->RemoveFromGameplay();
-		AmmoInstances.Push(AmmoInstance);
-	}
-	
-	for (UActorComponent* Component : GetComponentsByInterface(ULimenInteractableComponent::StaticClass()))
-	{
-		ILimenInteractableComponent* InteractableComponent = Cast<ILimenInteractableComponent>(Component);
-		check(InteractableComponent != nullptr);
-		InteractableComponent->GetInteractionDelegate()->AddUObject(this, &ALimenAmmoBox::Interact);
-	}
-}
-
 void ALimenAmmoBox::Interact(AController* InController, APawn* InPawn)
 {
-	RemoveFromGameplay();
+	Super::Interact(InController, InPawn);
 	
 	ULimenInteractionComponent* InteractionComponent = InPawn->GetComponentByClass<ULimenInteractionComponent>();
-	check(InteractionComponent != nullptr);
-
-	for (ALimenAmmo* Instance : AmmoInstances)
+	if (InteractionComponent == nullptr)
 	{
-		InteractionComponent->Interact(InController, InPawn, Instance);
+		return;
+	}
+
+	FActorSpawnParameters Params;
+	Params.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+	for (int i = 1; i < AmmoCount; i++)
+	{
+		ALimenAmmo* Ammo = GetWorld()->SpawnActor<ALimenAmmo>(AmmoClass, Params);
+		InteractionComponent->Interact(InController, InPawn, Ammo);
 	}
 }
