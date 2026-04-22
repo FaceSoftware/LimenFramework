595e8712b1891b0370ab339b9f9bca3d7feaa6ec
José Parente <j.eduardo.parente@gmail.com>
2026-04-22 20:04:16 +0100

    Add copyright notices

--- LimenCore/Source/LimenCore/Private/GridArray/GridArray.cpp
+++ LimenCore/Source/LimenCore/Private/GridArray/GridArray.cpp
@@ -1,2 +1,2 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Copyright FaceSoftware. All rights reserved.
 
--- LimenInteraction/Source/LimenInteraction/Private/Components/LimenSlotInventoryComponent.cpp
+++ LimenInteraction/Source/LimenInteraction/Private/Components/LimenSlotInventoryComponent.cpp
@@ -1,4 +1,4 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Copyright FaceSoftware. All rights reserved.
 
 
 #include "Components/LimenSlotInventoryComponent.h"
